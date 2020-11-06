a7dd11c341a153d425c0cbf07bc66758e7cadf2d
Uthsavikp <uthsavi29@gmail.com>
2020-11-07 03:01:17 +0530

    Added:To Caluclate A Condition Of Total Working Hours Or Days Per Month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -82,3 +82,34 @@ do
 		salary=$(($empHrs*$empRatePerHr));
 		totalSalary=$(($totalSalary+$salary));
 done
+
+
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && 
+	$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3));
+	case $empCheck in
+       	    $isFullTime)
+	    	empHrs=8
+	    	;;
+	    $isPartTime)
+	    	empHrs=4
+		;;
+	    *)
+	    	empHrs=0
+	     	;;
+	esac
+	totalEmpHrs=$(($totalEmpHrs*$empHrs))
+
+done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+
