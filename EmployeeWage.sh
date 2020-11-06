bbbec2a741b142c80dcccb2cd4523ff140748522
Uthsavikp <uthsavi29@gmail.com>
2020-11-07 01:21:46 +0530

    Added:To Solve Using Switch Case Statement

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -41,3 +41,22 @@ else
 	empHrs=0;
 fi
 salary=$(($empHrs*$empRatePerHr))
+
+
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$((RANDOM%3))
+
+case $empCheck in 
+	$isFullTime)
+		empHrs=8
+		;;
+	$isPartTime)
+		empHrs=4
+		;;
+	*)
+	empHrs=0
+		;;
+esac
+salary=$(($empHrs*$empRatePerHr));
